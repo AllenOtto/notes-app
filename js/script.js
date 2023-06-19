6986eb3cf71c79298c7f43ed02e3b3cfb6e63b71
AllenOtto <kephamoturi@gmail.com>
2023-06-19 09:20:27 +0300

    Degugging; enabled addition of user input to note object

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('[data-form]');
-const note = document.querySelector('[data-input]').value;
+const note = document.querySelector('[data-input]');
 const output = document.querySelector('[data-output]');
+
+// Added storage for our note objects
+let noteArr = [];
 
 form.addEventListener("submit", (e) => {
     // Prevent page refresh
@@ -8,22 +11,30 @@ form.addEventListener("submit", (e) => {
     // Generate a random Id for our Note class instance
     let id = generateId(); 
     // Create our object and pass it our random id and note (user input)
-    const noteObj = new Note(id, note);
-    noteObj.displayNote();
+    const noteObj = new Note(id, note.value);
+    // Add note objects to note array using the spread operator
+    noteArr = [...noteArr, noteObj];
+    // Display our note
+    Note.displayNote(noteArr);
 });
 
+// Blueprint for defining the characteristics and behaviour
+// of instances of our Note class
 class Note {
     constructor(id, note) {
         this.id = id;
         this.note = note;
     }
 
-    displayNote() {
+
+    static displayNote(list) {
         // Display our note on the frontend
         output.innerHTML = "<p>Going OUT</p>";
+        console.log(list);
     }
 }
 
+// Function definition of our note-item-id generator
 function generateId() {
-        return Math.random() * 1000000;
-    }
+    return Math.random() * 1000000;
+}
